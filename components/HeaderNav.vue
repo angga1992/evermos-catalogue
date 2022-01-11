a28b7ec724718a50a9d09213e0391ffd7576c4ad
anggamulya <anggamulya.ghazy@gmail.com>
2022-01-11 09:13:26 +0700

    add logo

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="header-nav">
     <div class="logo">
-      <img width="70px" height="70px" src="https://scontent-cgk1-1.xx.fbcdn.net/v/t39.30808-6/240213788_899490397356262_1783322661913117814_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE8oBY9Nykl99IiFJwRXE8-r1p8z_1jZ5SvWnzP_WNnlL-VY7vQBa3vFkVyUJDpYuo&_nc_ohc=aXuaCXYaOoEAX-cnhYd&_nc_ht=scontent-cgk1-1.xx&oh=00_AT_iKz15rWthxRU_rZsYfUanUuIuTg1oyEV5BFmkx03IsQ&oe=61DC5EC1" alt="">
+      <img width="70px" height="70px" :src="logo" alt="">
     </div>
     <div class="wrap">
       <div class="search">
@@ -21,10 +21,12 @@
 </template>
 
 <script>
+import logoevermos from '~/assets/img/logo.jpg'
 export default {
   data () {
     return {
-      search: ''
+      search: '',
+      logo: logoevermos
     }
   },
   methods: {
